15340bfb59651781d9659a5ef6b88789b0b554b6
Marconnedev <Marconnedev@gmail.com>
2025-03-14 15:22:02 -0300

    Primeiro commit

---
+++ workspace-spring-tool-suite/api/src/main/java/com/example/demo/controler/EstudanteController.java
@@ -0,0 +1,8 @@
+package com.example.demo.controler;
+
+import org.springframework.annotation.RequestMapping;
+import org.springframework.annotation.RestController;
+
+public class EstudanteController {
+
+}
